ba8eac2b87b8df5033e648ee8ff79c472676e5ed
Lado <lado.asambadze1@gmail.com>
2024-12-18 18:34:41 +0400

    trying add prod mode

---
+++ config/env/production/server.ts
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    app: {
+      keys: [
+        "your_very_secret_key_1",
+        "another_very_secret_key_2",
+        "and_a_third_very_secret_key_3",
+      ],
+    },
+    url: env("PUBLIC_URL", "https://api.daudtravel.com"),
+  });
+  
