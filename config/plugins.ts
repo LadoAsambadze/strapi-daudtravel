d66f010b5c4d4fce321a729742917e2b07366761
Lado <lado.asambadze1@gmail.com>
2024-12-18 16:59:03 +0400

    create new api with graphql

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    email: {
+        config: {
+          provider: 'nodemailer',
+          providerOptions: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: 'lado.asambadze1@gmail.com',
+              pass: 'yrpdlftdwodjkiqr', // Your app password
+            },
+          },
+          settings: {
+            defaultFrom: 'lado.asambadze1@gmail.com',
+            defaultReplyTo: 'lado.asambadze1@gmail.com',
+          },
+        },
+      },
+    // ...
+  });
